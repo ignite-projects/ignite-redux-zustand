9e2e9ffaf8bc7bb84f71df95a8695724323cc21b
Robson Natanael <natanaelrobson@gmail.com>
2024-01-08 15:18:26 -0300

    refactor: components obsoletes removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./store";
-import { TodoList } from "./components/TodoList";
-import { AddTodo } from "./components/AddTodo";
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
+      <p>Player</p>
     </ReduxProvider>
   );
 }
